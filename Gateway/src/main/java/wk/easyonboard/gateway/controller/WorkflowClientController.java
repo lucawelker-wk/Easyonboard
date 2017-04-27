b68913d2446864c8358b1ac8d271e69f9a47bec3
Luca Welker <luca.welker@wolterskluwer.com>
2017-04-27 10:15:16 +0200

    Reimplemented Workflow /Start

--- Gateway/src/main/java/wk/easyonboard/gateway/controller/WorkflowClientController.java
+++ Gateway/src/main/java/wk/easyonboard/gateway/controller/WorkflowClientController.java
@@ -1,52 +1,26 @@
 package wk.easyonboard.gateway.controller;
 
-import wk.easyonboard.common.datatransfer.WorkflowDTO;
-
-import javax.ws.rs.*;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Luca Welker on 4/26/17.
  */
-@Path("/api/workflows")
+@Path("/api/workflow/{workflowId}")
 public class WorkflowClientController extends BaseClientController {
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<WorkflowDTO> getWorkflows() {
-        return buildAdminClient()
-                .path("workflows")
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .buildGet()
-                .invoke(new GenericType<List<WorkflowDTO>>() {
-                });
-    }
-
-    @Path("/count")
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Integer getWorkflowCount() {
-        return buildAdminClient()
-                .path("workflows")
-                .path("count")
-                .request(MediaType.TEXT_PLAIN)
-                .buildGet()
-                .invoke(Integer.class);
-    }
-
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public boolean createWorkflow(WorkflowDTO workflow) {
-        Response response = buildAdminClient()
-                .path("workflows")
+    @Path("start/{employeeId}")
+    public UUID startWorkflow(@PathParam("workflowId") UUID workflowId, @PathParam("employeeId") UUID employeeId) {
+        return buildWorkflowClient()
+                .path("workflow")
+                .path(workflowId.toString())
+                .path("start")
+                .path(employeeId.toString())
                 .request(MediaType.APPLICATION_JSON_TYPE)
-                .buildPost(Entity.entity(workflow, MediaType.APPLICATION_JSON_TYPE))
-                .invoke();
-
-        return response.getStatusInfo().getStatusCode() == 200;
+                .buildPost(null)
+                .invoke(UUID.class);
     }
 }
--- WorkflowService/src/main/java/wk/easyonboard/workflowservice/controller/WorkflowController.java
+++ WorkflowService/src/main/java/wk/easyonboard/workflowservice/controller/WorkflowController.java
@@ -7,13 +7,13 @@ import java.util.UUID;
 /**
  * Created by Luca Welker on 4/26/17.
  */
-@Path("/workflow")
+@Path("/workflow/{workflowId}")
 public class WorkflowController {
-    @Path("/start")
+    @Path("/start/{employeeId}")
     @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public UUID startWorkflow(@FormParam("workflowId") String workflowId, @FormParam("employeeId") String employeeId) {
+    public UUID startWorkflow(@PathParam("workflowId") String workflowId, @PathParam("employeeId") String employeeId) {
         return UUID.randomUUID();
     }
 }
